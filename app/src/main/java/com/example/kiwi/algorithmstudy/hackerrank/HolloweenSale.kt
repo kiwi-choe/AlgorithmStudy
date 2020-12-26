b472a94754ba07ad01c9f699b286299674edffc7
kiwi-choe <magmag0221@gmail.com>
2020-12-26 15:58:59 +0900

    [ADD] Q1. https://www.hackerrank.com/challenges/halloween-sale/problem Q2. https://programmers.co.kr/learn/courses/30/lessons/64061?language=kotlin

---
+++ app/src/main/java/com/example/kiwi/algorithmstudy/hackerrank/HolloweenSale.kt
@@ -0,0 +1,30 @@
+package com.example.kiwi.algorithmstudy.hackerrank
+
+
+fun holloweenSaleSolve(price: Int, discountValue: Int, minValue: Int, budget: Int): Int {
+    var totalBudget = budget
+    var changingPrice = price
+    var count = 0
+    // end condition
+    while (totalBudget > 0) {
+        if (changingPrice <= minValue) {
+            if (totalBudget < minValue) {
+                break
+            }
+            println("price$minValue, totalBudget$totalBudget")
+            totalBudget -= minValue
+        } else {
+            totalBudget -= changingPrice
+            println("price$changingPrice, totalBudget$totalBudget")
+            changingPrice -= discountValue
+        }
+        count++
+    }
+    return count
+}
+
+fun main(args: Array<String>) {
+    val result = holloweenSaleSolve(20, 3, 6, 80)
+
+    println("result: $result")
+}
---
+++ app/src/main/java/com/example/kiwi/algorithmstudy/hackerrank/PickDollGame.kt
@@ -0,0 +1,48 @@
+package com.example.kiwi.algorithmstudy.hackerrank
+
+fun solution(board: Array<IntArray>, moves: IntArray): Int {
+    var answer = 0
+    val basket = arrayListOf(0)
+
+    val rowLength = board.size
+//    val colLength = board[0].size
+    moves.forEach {
+        val colIndex = it - 1
+        println("colIndex: $colIndex")
+        for (i in 0 until rowLength) {
+            val number = board[i][colIndex]
+            println("board[$i][$colIndex]: $number")
+
+            // break condition 2
+            if (number != 0) {
+                // break condition 1
+                if (number == basket.last()) {
+                    basket.removeAt(basket.lastIndex)   // asis: list.remove(o: Object) Coz of the complexity of time it has not passed the test 7~10
+                    answer += 2
+                } else {
+                    basket.add(number)
+                }
+                // pop value; init 0
+                board[i][colIndex] = 0
+                break
+            }
+        }
+    }
+//    println("basket: $basket")
+    // row of board loop
+    return answer
+}
+
+fun main(args: Array<String>) {
+    val ar = intArrayOf(0, 0, 0, 0, 0)  // row 0
+    val ar2 = intArrayOf(0, 0, 1, 0, 3) // row 1
+    val ar3 = intArrayOf(0, 2, 5, 0, 1)
+    val ar4 = intArrayOf(4, 2, 4, 4, 2)
+    val ar5 = intArrayOf(3, 5, 1, 3, 1) // ... row 4
+
+    val board = arrayOf(ar, ar2, ar3, ar4, ar5)
+    val moves = intArrayOf(1, 5, 3, 5, 1, 2, 1, 4)
+    val result = solution(board, moves)
+
+    println("result: $result")
+}
